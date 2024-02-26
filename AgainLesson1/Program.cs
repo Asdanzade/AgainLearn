d6eb069bd439ae9ddfcac3552175125e1096a547
Islam <iesdanzade@gmail.com>
2024-02-26 23:26:50 +0400

    Project initially added

---
+++ AgainLesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AgainLesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meraba arkadeşlerrr hahaha!");
+        }
+    }
+}
